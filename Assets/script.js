da3508825ff6885ce5513c92ad54027f10e37e3a
Wes Young <wesley.young@du.edu>
2020-10-16 17:34:59 -0600

    Completed javascript up to reset with jQuery.  Resetting code to vanilla JS next.

--- Assets/script.js
+++ Assets/script.js
@@ -4,35 +4,56 @@
 //Fourth make them interchangable
 //Fifth Clean up
 var totalTime = 30;
-function startTimer() {
-  setInterval(function () {
-    totalTime--;
-    $("#timeDisplay").text(totalTime);
-  }, 1000);
-}
-
-$("#startButton").click(function () {
-  setInterval(function () {
-    totalTime--;
-    $("#timeDisplay").text(totalTime);
-  }, 1000);
-  $("#startButton").attr("style", "visibility:hidden");
-  startQuiz();
-});
+var userScore = 0;
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var questionNumber = 0;
 var questionArray = [
   "First Question",
   "Second Question",
   "Third Question",
   "Fourth Question",
   "Fifth Question",
+  "Sixth Question",
+  "7th Question",
+  "8th Question",
+  "9th Question",
+  "10th Question",
 ];
 var answerArray = [
   { "The wofj owfjo": true, op: false, yo: false, omk: false },
   { pop: false, ye: true, lp: false, mm: false },
+  { pop: false, ye: true, lp: false, megm: false },
+  { pop: false, ye: true, lp: false, mm: false },
+  { pop: false, ye: true, lp: false, mmeg: false },
+  { pop: false, ye: true, lp: false, mm: false },
+  { pop: false, ye: true, lp: false, megm: false },
+  { pop: false, ye: true, lp: false, mm: false },
+  { pop: false, ye: true, lp: false, mm: false },
+  { pop: false, ye: true, lp: false, mm: false },
 ];
 
-questionNumber = 0;
+$("body").on("click", "#startButton", function () {
+  var quizTimer = setInterval(function () {
+    totalTime--;
+    $("#timeDisplay").text(totalTime);
+
+    if (totalTime === 0) {
+      clearInterval(quizTimer);
+      showResult();
+    }
+
+    if (questionNumber === questionArray.length + 1) {
+      clearInterval(quizTimer);
+      showResult();
+    }
+  }, 1000);
+  $("#startButton").attr("style", "visibility:hidden");
+  startQuiz();
+});
+
 function startQuiz() {
+  $("#startButton").remove();
   $("#quizBox").append("<h1>" + questionArray[0] + "</h1>");
   var answerButtonHolder = $("<div>");
   var button1 = $("<button>");
@@ -79,6 +100,7 @@ $("#quizBox").on("click", ".answerButton", function () {
 
 function NextQuestion() {
   if (questionNumber === questionArray.length) {
+    questionNumber++;
     return;
   }
   $("h1").text(questionArray[questionNumber]);
@@ -92,6 +114,51 @@ function NextQuestion() {
 
 function CheckCorrect() {
   if (answerArray[questionNumber - 1][selectedAnswer]) {
-    alert("You are correct");
+    userScore = userScore + 5;
+    correctAnswer++;
+  } else {
+    wrongAnswer++;
   }
 }
+
+function showResult() {
+  $("#timeDisplay").attr("style", "visibility:hidden");
+  $(".answerButton").remove();
+  var resultDisplay = $(
+    "<h3>" +
+      "Your total score was " +
+      userScore +
+      "</h3>" +
+      "<h3>" +
+      "Your total number correct was " +
+      correctAnswer +
+      "</h3>" +
+      "<h3>" +
+      "Your total number wrong was " +
+      wrongAnswer +
+      "</h3>"
+  );
+  $("body").append(resultDisplay);
+  resetButton = $("<button>");
+  resetButton.text("Reset Quiz");
+  resetButton.attr("id", "resetButton");
+  $("#quizBox").append(resetButton);
+  highScore = $("<button>");
+  highScore.text("High Score Board");
+  highScore.attr("id", "highScore");
+  $("#quizBox").append(highScore);
+}
+
+$("#quizBox").on("click", "#resetButton", function () {
+  $("#resetButton").remove();
+  $("#highScore").remove();
+  $("h1").remove();
+  $("h3").remove();
+  $("h2").attr("style", "visibility:visible");
+  var startButton = $("<button>");
+  startButton.text("Start");
+  startButton.attr("id", "startButton");
+  $("#quizBox").append(startButton);
+  questionNumber = 0;
+  totalTime = 30;
+});
